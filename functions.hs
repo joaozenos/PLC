1eecf2e2b4979840a9efebb287bac48b259c2c38
Joao Luiz de Andrade Neto <jlan@ug3c13.windows.cin.ufpe.br>
2017-03-14 11:36:29 -0300

    digits, sumPairs, fat, fib, listFib - Start maiorList

--- functions.hs
+++ functions.hs
@@ -42,12 +42,44 @@ sumList as
 double :: [Int] -> [Int]
 double ak
   | ak == [] = []
-  | otherwise = [head ak * 2] ++ double(tail ak)
+  | otherwise = head ak *2 : double(tail ak)
   
 membership :: [Int] -> Int -> Bool
 membership ab v
   | tail ab == [] && head ab == v = True
   | tail ab == [] && head ab /= v = False
   | otherwise = membership([head ab]) v || membership(tail ab) v 
-  
+
 digits :: String -> String
+digits x
+  | x == [] = []
+  | head x >= '0' && head x <= '9' = digits (tail x)
+  | otherwise = head x : digits(tail x)
+
+sumPairs :: [Int] -> [Int] -> [Int]
+sumPairs a b
+  | length a /= length b = error "Diferent list size"
+  | a == [] || b == [] = []
+  | otherwise = head a + head b : sumPairs (tail a) (tail b)
+
+maiorLista :: [Int] -> Int
+maiorLista [] = minBound :: Int
+maiorLista [x] = x
+
+fat :: Int -> Int
+fat x
+  | x == 0 = 1
+  | otherwise = fat (x-1) * x
+
+fib :: Int -> Int
+fib x
+  | x == 0 = 0
+  | x == 1 = 1
+  | otherwise = fib(x-1) + fib(x-2) 
+
+listFib :: Int -> Int -> [Int]
+listFib n x 
+  | n == 0 = []
+  | mod (fib (x)) 2 == 0 = fib(x):listFib (n-1) (x+1)
+  | otherwise = listFib(n) (x+1) 
+
